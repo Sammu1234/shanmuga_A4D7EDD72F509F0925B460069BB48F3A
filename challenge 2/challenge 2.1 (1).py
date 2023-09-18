2f10974551a3c721f072a551739afa7009024f4b
Sammu1234 <144781815+Sammu1234@users.noreply.github.com>
2023-09-18 09:55:02 +0530

    Update challenge 2.1 (1).py

--- challenge 2/challenge 2.1 (1).py
+++ challenge 2/challenge 2.1 (1).py
@@ -19,7 +19,7 @@ class bankaccount:
   def display_balance(self):
     print("Account balance for {} (Account #{}): ₹{}.".format(self.__account_holder_name,self.__account_number,self.__account_balance))
 
-account = bankaccount(account_number="123456789",account_holder_name="hari prasad",initial_balance=5000.0)
+account = bankaccount(account_number="123456789",account_holder_name="shanmuga",initial_balance=5000.0)
 account.display_balance()
 account.deposit(500.0)
 account.withdraw(200.0)
